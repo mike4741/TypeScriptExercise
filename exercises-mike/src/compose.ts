6376fabfdb1c0f3f03389d72a079e52a6fb082c9
mike4741 <64281489+mike4741@users.noreply.github.com>
2021-04-19 12:00:53 -0500

    final update

--- exercises-mike/src/compose.ts
+++ exercises-mike/src/compose.ts
@@ -1,26 +1,31 @@
 /*
 Make this compose function work.
 NOTE: Allow it to accept an arbitrary number of arguments.
-
-Refactor to allow it to accept any number of arguments
+Refactor to allow it to accept any number of arguments.
 */
 
- //  function  that accept any number of function as an argument and return the result as of f(g(h(x)))
+//This function accept any number of function and return the output . Example like f(g(h(x))))
+
+
 function compose(...funs:any[]) {
     return function(x: any) {
-        for (let i = funs.length - 1; i >= 0; i--) {
+        for (let i:number = funs.length - 1; i >= 0; i--) {
             x = funs[i](x);
         }
         return x;
     }
 }
 
-// functions that add , negate  and double the value
+
+
+/*  for test case   their are three function  
+        double : that double the number it accept 
+        negate : that negate the number it accept 
+        add : that add 10 to the  number it accept 
+*/
 const double =function (a:number) { return 2*a;}
 const negate = function (a:number) { return -a;}
-const add = function (a:number) { return a+10;}
+const add = function (a:number) {return a+10}
 
-
-// test output
-console.log(compose (double, negate,add)(5));//-30
-
+// test output 
+console.log(compose (double, negate,add)(5)); //-30
